d8cea83adb2da05f8de6814af0c7d73ce51ad862
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-03-06 22:36:41 +0100

    Esercizio completato

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 // COLLEGAMENTO VUEJS
 const app = new Vue({
-    el: '#root',
-    data: {
-        email: 10,
-        emailList: [],
-    },
-    created() {
-
-        for (let i = 0; i < this.email; i++) {
-
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
-
-                if (res.data.succes) {
-                    this.emailList.push(res.data.response)
-                    console.log(this.emailList)
-                }
-
-            })
-
-        };
-
-    },
+  el: "#root",
+  data: {
+    email: 10,
+    emailList: [],
+  },
+  created() {
+    for (let i = 0; i < this.email; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((res) => {
+          if (res.data.success) {
+            this.emailList.push(res.data.response);
+            console.log(this.emailList);
+          }
+        });
+    }
+  },
 });
 
 console.log(app);
